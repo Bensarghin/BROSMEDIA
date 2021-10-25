ee76429685e8f5277e1156e5bc6993a89a7abd24
Bensarghin <hm.bensarghin@gmail.com>
2021-10-25 20:06:41 +0100

    pull from last commit

--- resources/views/admin_pages/rendey-vous/manage.blade.php
+++ resources/views/admin_pages/rendey-vous/manage.blade.php
@@ -10,35 +10,35 @@
         </div> 
     </div>
     <div class="card-body">
-        <div class="table-responsive">
-            <table class='table table-light table-hover'>
-                <tr>
-                    
-                    <td>PATIENT</td>
-                    <td>DATE PREND RDV</td>
-                    <td>DATE CONSULTATION</td>
-                    <td>ACTE</td>
-                    <td>MAJ</td>
-                </tr>
-                @foreach ($data as $rdv)
-                <tr>
-                    
-                    <td>{{$rdv->nom}} {{$rdv->prenom}} </td>
-                    <td>{{$rdv->date_prend_rdv}}</td>
-                    <td>{{$rdv->date_consu}}</td>
-                    <td>{{$rdv->nom_acte}}</td>
-                    <td>
-                        <a href="" class="text-primary"><i class="fas fa-calendar"></i> ajout rdv</a> |
-                        <a href="{{route('rdv.update',['id'=>$rdv->id])}}" class="text-info"><i class="fas fa-edit"></i> mod</a> | 
-                        <a onclick="return confirm('Vous étes vraiment à supprimer ce enregistrement')" class="text-danger" href="{{route('patient.delete',['id'=>$rdv->id])}}"><i class="fas fa-trash"></i> sup</a>
-                    </td>
-                    
-                </tr>
-                @endforeach
-            </table>
-        </div>
+        <table class='table table-striped table-bordered'>
+            <tr>
+                
+                <td>PATIENT</td>
+                <td>DATE PREND RDV</td>
+                <td>DATE CONSULTATION</td>
+                <td>ACTE</td>
+                <td>MAJ</td>
+            </tr>
+            @foreach ($data as $rdv)
+            <tr>
+                
+                <td>{{$rdv->nom}} {{$rdv->prenom}} </td>
+                <td>{{$rdv->date_prend_rdv}}</td>
+                <td>{{$rdv->date_consu}}</td>
+                <td>{{$rdv->nom_acte}}</td>
+                <td>
+                    <a href="" class="text-primary"><i class="fas fa-calendar"></i> ajout rdv</a> |
+                    <a href="{{route('rdv.update',['id'=>$rdv->id])}}" class="text-info"><i class="fas fa-edit"></i> mod</a> | 
+                    <a onclick="return confirm('Vous étes vraiment à supprimer ce enregistrement')" class="text-danger" href="{{route('patient.delete',['id'=>$rdv->id])}}"><i class="fas fa-trash"></i> sup</a>
+                </td>
+                
+            </tr>
+            @endforeach
+        </table>
+    </div>
+</div>
         <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
         <script>
-
+            
         </script>
 @endsection
